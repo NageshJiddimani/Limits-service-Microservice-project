6a0dbe7cd8e953a7493eb3d2f795d63ce08b9afd
2111568 <2111568@LTIN448705.cts.com>
2023-08-21 18:15:19 +0530

    Limit services

---
+++ limits-service/src/test/java/com/nagesh/limits/LimitsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagesh.limits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
